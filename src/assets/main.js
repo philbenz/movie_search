1223129bbaf70534df98afe972b84cf4358ea4af
philbenz@gmail.com <philbenz@Morrhysseys-MacBook-Air.local>
2016-07-26 10:05:26 -0600

    unscrewing up the js file

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,4 +4,23 @@ $(document).on('ready', function() {
 
     //sanity check
     console.log('sanity check with movie search js');
+
+
+    //jQuery for the movie title
+    $( ".submit" ).click(function( event ) {
+      //ensure that the submit doesn't go to a server
+      event.preventDefault();
+      pullMovieDB($('.movie_title').val());
+    });
+  }); // closes the document.on
+
+
+function pullMovieDB (title) {
+  $.ajax({
+    method: 'GET',
+    url: 'http://www.omdbapi.com/' + title
+  }).done(function(movie){
+    console.log("Movie Info:  " + movie);
   });
+
+}
