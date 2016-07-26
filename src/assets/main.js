030f3c21f7b642abb74c987716a232ddebe7516e
philbenz@gmail.com <philbenz@Morrhysseys-MacBook-Air.local>
2016-07-26 09:19:07 -0600

    with alan kicking it out

---
+++ src/assets/main.js
@@ -0,0 +1,7 @@
+$(document).on('ready', function() {
+    //prevent the event default
+    // event.preventDefault();
+
+    //sanity check
+    console.log('sanity check with movie search js');
+  });
